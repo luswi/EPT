6cd70f18f6cbaec00f277ab12f5de0bfe81503b0
luswi <lukasz.swiercz@gmail.com>
2018-08-16 23:02:33 +0200

    beta

---
+++ EPT/EPT.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EPT.classes;
+
+namespace EPT
+{
+    public partial class EPT : Form
+    {
+        public EPT()
+        {
+            InitializeComponent();
+        }
+        seaClass v = new seaClass();
+
+        private void calculateBT_Click(object sender, EventArgs e)
+        {
+            //Pick up values from textboxes
+            v.zdk = double.Parse(zdkTB.Text);
+            //v.test = double.Parse(testTB.Text);
+
+            //results
+            bool success = v.seaCalculation(v);
+            if (success == true)
+            {
+                testTB.Text = Convert.ToString(v.zdk);
+            }
+            else
+            {
+                MessageBox.Show("cos poszlo zle");
+            }
+        }
+    }
+}
---
+++ EPT/classes/seaClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPT.classes
+{
+    public class seaClass
+    {
+        // Variables used for calculations
+        public double zdk { get; set; }
+        
+
+
+        public bool seaCalculation(seaClass v)
+        {
+            bool isSuccess = false;
+            if (1 > 0)
+            {
+                isSuccess = true;
+                //here calc
+            }
+            else
+            {
+                isSuccess = false;
+            }
+            return isSuccess;
+        }
+    }
+}
